a0592fb2181c893d0027c436d87e7794a25f830c
Deewens <deeewens@gmail.com>
2020-02-15 19:31:46 +0100

    Test RMI

--- RMIGamesClient/src/iut/progrep/games/rmi/Client.java
+++ RMIGamesClient/src/iut/progrep/games/rmi/Client.java
@@ -2,15 +2,22 @@ package iut.progrep.games.rmi;
 
 import java.rmi.Naming;
 
+import iut.progrep.games.TicTacToeInterface;
+import iut.progrep.games.pojo.Joueur;
+
 public class Client {
 	public static void main(String[] args) {
 		try {
 			int port = 8000;
 			
 			// rmi://localhost:port/nomService
+			
+			SecurityManager security = System.getSecurityManager();
 
-			TicTacToeInterface tictactoe = (TicTacToeInterface) Naming.lookup("rmi://localhost:" + port + "tictactoe");
-			MatchesInterface matches = (MatchesInterface) Naming.lookup("rmi://localhost:" + port + "/matches");
+			TicTacToeInterface tictactoe = (TicTacToeInterface) Naming.lookup("rmi://localhost:" + port + "/tictactoe");
+			
+			Joueur j1 = new Joueur("Deewens");
+			tictactoe.rejoindrePartie(j1);
 		} catch(Exception e) {
 			System.out.println("Erreur lors de l'initialisation du client : " + e);
 		}
